408c5b16539165084df0bd0f24972110226a8951
aleksas <aasmc@yandex.ru>
2022-10-18 12:16:05 +0300

    Add items 12 and 13 examples

---
+++ src/main/java/ru/aasmc/effectivejava/chapter_03/item_14/HashTable.java
@@ -0,0 +1,46 @@
+package ru.aasmc.effectivejava.chapter_03.item_14;
+
+public class HashTable implements Cloneable {
+
+    // stub for example purposes
+    private Entry[] buckets = new Entry[1024];
+
+    private static class Entry {
+        final Object key;
+        Object value;
+        Entry next;
+
+        public Entry(Object key, Object value, Entry next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        /**
+         * Stack safe deepCopy method.
+         */
+        Entry deepCopy() {
+            Entry result = new Entry(key, value, next);
+            for (Entry p = result; p.next != null; p = p.next) {
+                p.next = new Entry(p.next.key, p.next.value, p.next.next);
+            }
+            return result;
+        }
+    }
+
+    @Override
+    public HashTable clone() {
+        try {
+            HashTable result = (HashTable) super.clone();
+            result.buckets = new Entry[buckets.length];
+            for (int i = 0; i < buckets.length; i++) {
+                if (buckets[i] != null) {
+                    result.buckets[i] = buckets[i].deepCopy();
+                }
+            }
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
